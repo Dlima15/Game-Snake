8508bb4a81bcc9783a3a060cca468d4c786eab51
Dlima15 <danilo.lima7456@gmail.com>
2023-11-13 17:06:45 -0300

    Function fechar jogo

--- Snake-D.py
+++ Snake-D.py
@@ -5,6 +5,7 @@ pygame.init()
 pygame.display.set_caption("Jogo da Cobrinha")
 largura, altura = 600, 400
 pygame.display.set_mode((largura, altura))
+relogio = pygame.time.Clock()
 
 #cores dos elementos, cobrinha, comida e fundo
 
@@ -17,4 +18,35 @@ azul = (0, 0, 255)
 #medidas da cobrinha
 
 tamanho_quadrado = 10
-velocidade = 
+velocidade = 15 
+
+#inicio do código do jogo
+
+def jogar_jogo():
+    fim_do_jogo = False
+
+
+    while not fim_do_jogo:
+        
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                fim_do_jogo = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+jogar_jogo()
