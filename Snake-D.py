09ebe0347caafab81a3e36a57ec7a2051b7f492b
Dlima15 <danilo.lima7456@gmail.com>
2023-11-13 16:56:04 -0300

    variaveis do jogo Snake

---
+++ Snake-D.py
@@ -0,0 +1,20 @@
+import random
+import pygame
+
+pygame.init()
+pygame.display.set_caption("Jogo da Cobrinha")
+largura, altura = 600, 400
+pygame.display.set_mode((largura, altura))
+
+#cores dos elementos, cobrinha, comida e fundo
+
+preta = (0, 0, 0)
+branca = (255, 255, 255)
+vermelha = (255, 0, 0)
+verde = (0, 255, 0)
+azul = (0, 0, 255)
+
+#medidas da cobrinha
+
+tamanho_quadrado = 10
+velocidade = 
